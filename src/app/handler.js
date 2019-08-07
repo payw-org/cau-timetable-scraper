b34d6a4482ae13680d6bfc02a3a1e7ccab69bf13
hchangjae <hcj@payw.org>
2019-08-07 21:34:15 +0900

    have a taste of node-schedule

--- src/app/handler.js
+++ src/app/handler.js
@@ -1,1 +1,18 @@
-a
+const axios = require('axios')
+const appRoot = require('app-root-path')
+const schedule = require('node-schedule')
+
+const processedData = require(appRoot +
+  '/src/resource/parsed/2019-2-학부-서울--done')
+
+axios({
+  url: 'https://api.eodiro.com/aaaaa',
+  method: 'get',
+  data: {
+    result: processedData
+  }
+})
+
+const scrapSchedule = schedule.scheduleJob('10 * * * *', function() {
+  console.log('매 10초에 실행')
+})
