89c71b542a53c0c9837d7db667fe900e75042178
hchangjae <hcj@payw.org>
2019-08-07 18:40:35 +0900

    rename apps

---
+++ src/app/duplicate-deletor.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const appRoot = require('app-root-path')
+const fileName = '/src/resource/scraped/2019-2-학부-서울--'
+
+var data = JSON.parse(fs.readFileSync(appRoot + fileName + '.json', 'utf-8'))
+
+function arrIndexOf(arr, item) {
+  var length = arr.length
+  for (var i = 0; i < arr.length; i++) {
+    if (
+      arr[i]['과목번호-분반'] == item['과목번호-분반'] &&
+      arr[i]['과목명'] == item['과목명'] &&
+      arr[i]['담당교수'] == item['담당교수'] &&
+      arr[i]['강의실/강의시간'] == item['강의실/강의시간']
+    ) {
+      break
+    }
+  }
+  return i
+}
+
+async function delete_duplicate(data) {
+  var a = new Array()
+
+  return await data.filter((item, index) => {
+    return arrIndexOf(data, item) == index
+  })
+}
+
+;(async function() {
+  data = await delete_duplicate(data)
+
+  fs.writeFileSync(appRoot + fileName + 'done.json', JSON.stringify(data))
+})()
---
+++ src/app/scraper.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer')
+const appRoot = require('app-root-path')
+const portal = require(appRoot + '/src/module/scrap-portal.js')
+const searchInfo = require(appRoot + '/src/config/searchInfo.json')
+const account = require(appRoot + '/src/config/info.json')
+
+;(async () => {
+  // creat new page
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+  const page = await browser.newPage()
+  await page.setViewport({
+    width: 1280,
+    height: 1080
+  })
+
+  // login portal
+  await portal.loginAdmin(page, account)
+
+  // move to timetable page
+  await page.goto(
+    'https://mportal.cau.ac.kr/std/usk/sUskSif001/index.do?type=1'
+  )
+  await page.waitForSelector('.nb-search-submit')
+
+  // search class list
+  var classListScraped = await portal.scrap_ClassList_All(page, searchInfo)
+
+  // save class list
+  await portal.saveClassList(classListScraped)
+
+  await browser.close()
+})()
