b5a15dbcdaca9aca1efa84e524024cb28daea351
hchangjae <hcj@payw.org>
2019-08-08 19:11:15 +0900

    if there is no change return null

--- src/app/scraper.js
+++ src/app/scraper.js
@@ -1,11 +1,13 @@
 const puppeteer = require('puppeteer')
 const appRoot = require('app-root-path')
+const fs = require('fs')
+
 const portal = require(appRoot + '/src/module/scrap-portal.js')
-const searchInfo = require(appRoot + '/src/config/searchInfo.json')
-const account = require(appRoot + '/src/config/info.json')
 
 module.exports.run = async () => {
   // creat new page
+  var searchInfo = JSON.parse(fs.readFileSync(appRoot + '/src/config/searchInfo.json','utf8'))
+  const account = JSON.parse(fs.readFileSync(appRoot + '/src/config/info.json','utf8'))
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
   await page.setViewport({
@@ -24,9 +26,20 @@ module.exports.run = async () => {
 
   // search class list
   var classListScraped = await portal.scrap_ClassList_All(page, searchInfo)
+  await browser.close()
+
+  // check exist file
+  var dataName = portal.getDataName(searchInfo)
+  if(fs.existsSync(appRoot + '/src/resource/scraped/' + dataName)){
+    var existData = fs.readFileSync(appRoot + '/src/resource/scraped/' + dataName, 'utf8')
+    var newData = JSON.stringify(classListScraped)
+    if(existData === newData){
+      return null
+    }
+  }
 
   // save class list
-  await portal.saveClassList(classListScraped)
+  await portal.saveClassList(classListScraped,dataName)
 
-  await browser.close()
+  return dataName
 }
