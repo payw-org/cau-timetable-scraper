4f45a35b261454994ada44776d9bd8f4cd145bc0
hchangjae <hcj@payw.org>
2019-08-08 15:34:43 +0900

    scheduling scraper,duplicate-deletor,parser pttype

--- src/app/handler.js
+++ src/app/handler.js
@@ -1,18 +1,26 @@
 const axios = require('axios')
 const appRoot = require('app-root-path')
-const schedule = require('node-schedule')
+const scraper = require(appRoot + '/src/app/scraper.js')
+const parser = require(appRoot + '/src/app/parser.js')
+const duplicateDeletor = require(appRoot + '/src/app/duplicate-deletor.js')
 
-const processedData = require(appRoot +
-  '/src/resource/parsed/2019-2-학부-서울--done')
+// const processedData = require(appRoot +
+//   '/src/resource/parsed/2019-2-학부-서울--done')
 
-axios({
-  url: 'https://api.eodiro.com/aaaaa',
-  method: 'get',
-  data: {
-    result: processedData
-  }
-})
+// axios({
+//   url: 'https://api.eodiro.com/aaaaa',
+//   method: 'get',
+//   data: {
+//     result: processedData
+//   }
+// })
 
-const scrapSchedule = schedule.scheduleJob('10 * * * *', function() {
-  console.log('매 10초에 실행')
-})
+var schedule = require('node-schedule');
+ 
+var j = schedule.scheduleJob('*/30 * * * * *', async function(){
+  console.log('Play!')
+  await scraper.run()
+  await duplicateDeletor.run()
+  parser.run()
+  console.log('Done')
+});
