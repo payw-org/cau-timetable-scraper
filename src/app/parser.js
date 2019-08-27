32d9012637fc01f8b2944f73d85645434fedd2b6
hchangjae <hcj@payw.org>
2019-08-27 19:23:47 +0900

    module optimization

--- src/app/parser.js
+++ src/app/parser.js
@@ -3,37 +3,30 @@ var fs = require('fs')
 
 const parseCau = require(appRoot + '/src/module/parse-cau.js')
 
+module.exports.run = fileName => {
+  const scrapedData = JSON.parse(
+    fs.readFileSync(appRoot + '/src/resource/scraped/' + fileName, 'utf8')
+  )
 
-module.exports.run = (fileName) => {
-  const scrapedData = JSON.parse(fs.readFileSync(appRoot + '/src/resource/scraped/' + fileName,'utf8'))
   var courseArray = new Array()
-
   var course
-  
-  var classId_DivNo
-  var className
-  var classInstructor
   var classRoom_Time
-  var classRoom
-  
+
   for (i = 0; i < scrapedData.length; i++) {
     course = new Object()
-    classId_DivNo = scrapedData[i]['과목번호-분반']
-    className = scrapedData[i]['과목명']
-    classInstructor = scrapedData[i]['담당교수']
-    classRoom = scrapedData[i]['강의실/강의시간']
-  
-    classId_DivNo = classId_DivNo.split('-')
-    classRoom_Time = parseCau.parseClassRoom_Time(classRoom)
-  
+
+    classRoom_Time = parseCau.parseClassRoom_Time(
+      scrapedData[i]['강의실/강의시간']
+    )
+
     // main data
-    course.course_no = classId_DivNo[0]
-    course.class_no = classId_DivNo[1]
-    course.name = className
-    course.instructor = classInstructor
+    course.course_no = scrapedData[i]['과목번호-분반'].split('-')[0]
+    course.class_no = scrapedData[i]['과목번호-분반'].split('-')[1]
+    course.name = scrapedData[i]['과목명']
+    course.instructor = scrapedData[i]['담당교수']
     course.room = classRoom_Time[0]
     course.time = classRoom_Time[1]
-  
+
     // etc data
     course.college = scrapedData[i]['대학']
     course.subject = scrapedData[i]['개설학과']
@@ -51,10 +44,10 @@ module.exports.run = (fileName) => {
       courseArray.push(course)
     }
   }
-  
+
   courseArray = parseCau.parseToSend(courseArray)
-  
-  parseCau.createFile(courseArray,fileName)
-  
+
+  parseCau.createFile(courseArray, fileName)
+
   return fileName
 }
