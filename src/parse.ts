8afc304e1415a8ab342c16622ae91289d7f6307b
Jang Haemin <io@jhaemin.com>
2020-01-26 01:19:32 -0500

    feat: parse building and room

--- src/parse.ts
+++ src/parse.ts
@@ -1,9 +1,22 @@
 import { Lectures } from './types'
+import Print from './utils/print'
 
 export const parse = (lectures: Lectures) => {
   lectures.map(lecture => {
     const time = lecture.time
-    const building = time.match(/[0-9]+관/g)
-    const room = time.match(/[0-9]+호/g)
+
+    const buildingRegExp = /([0-9]+) *?관/g
+    const buildingMatch = buildingRegExp.exec(time)
+    if (buildingMatch) {
+      Print.ln(buildingMatch[1] + ` `)
+    }
+
+    const roomRegExp = /([0-9]+) *?호/g
+    const roomMatch = roomRegExp.exec(time)
+    if (roomMatch) {
+      Print.ln(roomMatch[1])
+    }
+
+    Print.ln('\n')
   })
 }
