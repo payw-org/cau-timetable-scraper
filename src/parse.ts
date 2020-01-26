ec6f1f817b681495c9e33612496abe22453a52ff
Jang Haemin <io@jhaemin.com>
2020-01-26 02:12:32 -0500

    feat: create regular expressions for parsing time

--- src/parse.ts
+++ src/parse.ts
@@ -3,20 +3,22 @@ import Print from './utils/print'
 
 export const parse = (lectures: Lectures) => {
   lectures.map(lecture => {
-    const time = lecture.time
+    const timeStr = lecture.time
 
     const buildingRegExp = /([0-9]+) *?관/g
-    const buildingMatch = buildingRegExp.exec(time)
+    const buildingMatch = buildingRegExp.exec(timeStr)
     if (buildingMatch) {
       Print.ln(buildingMatch[1] + ` `)
     }
 
     const roomRegExp = /([0-9]+) *?호/g
-    const roomMatch = roomRegExp.exec(time)
+    const roomMatch = roomRegExp.exec(timeStr)
     if (roomMatch) {
       Print.ln(roomMatch[1])
     }
 
-    Print.ln('\n')
+    const timeRegExp1 = /[월화수목금토일] *?\(?[0-9]+:[0-9]+~[0-9]+:[0-9]+\)?/g
+
+    const timeRegExp2 = /[월화수목금토일] *?([0-9] *?,?)+/g
   })
 }
