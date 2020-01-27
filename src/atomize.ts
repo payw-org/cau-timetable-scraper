d6b8f40d50c77f9e1ea60161d9cb938fdd3989b1
Jang Haemin <io@jhaemin.com>
2020-01-27 09:32:23 -0500

    feat: change the atomizing algorithm

--- src/atomize.ts
+++ src/atomize.ts
@@ -1,19 +1,37 @@
 import { Lectures } from './types'
 
 export const atomize = (lectures: Lectures) => {
-  const atomicLectures: Lectures = lectures.filter((lecture, index) => {
-    return (
-      index ===
-      lectures.findIndex(obj => {
-        return obj.code === lecture.code
+  let i = lectures.length
+
+  while (i--) {
+    const currentLecture = lectures[i]
+    const dupIndex = lectures.findIndex((lecture, index) => {
+      return (
+        index !== i &&
+        lecture.college === currentLecture.college &&
+        lecture.major === currentLecture.major &&
+        lecture.grade === currentLecture.grade &&
+        lecture.course === currentLecture.course &&
+        lecture.section === currentLecture.section &&
+        lecture.code === currentLecture.code &&
+        lecture.name === currentLecture.name &&
+        lecture.credit === currentLecture.credit &&
+        lecture.time === currentLecture.time &&
+        lecture.professor === currentLecture.professor &&
+        lecture.closed === currentLecture.closed &&
+        lecture.schedule === currentLecture.schedule &&
+        lecture.flex === currentLecture.flex &&
+        lecture.note === currentLecture.note
+      )
+    })
+
+    if (dupIndex !== -1) {
+      currentLecture.coverages.forEach(coverage => {
+        lectures[dupIndex].coverages.push(coverage)
       })
-    )
-  })
+      lectures.splice(i, 1)
+    }
+  }
 
-  return atomicLectures
-
-  // fs.writeFileSync(
-  //   `data/lectures-atomic.json`,
-  //   JSON.stringify(atomicLectures, null, 2)
-  // )
+  return lectures
 }
