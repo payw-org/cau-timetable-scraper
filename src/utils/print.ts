0529c6ed4f492db79f352d9a5aa2b73ec97fbc0d
Jang Haemin <io@jhaemin.com>
2020-03-26 09:23:13 +0900

    docs: add param descriptions and default values

--- src/utils/print.ts
+++ src/utils/print.ts
@@ -1,8 +1,18 @@
+/**
+ * Small utility for better printing
+ */
 export default class Print {
+  /**
+   * @param msg
+   * @param lineBreak Whether to line break or not @default false
+   */
   static ln(msg: string, lineBreak: boolean = false) {
     process.stdout.write(`${msg}${lineBreak ? '\n' : ''}`)
   }
 
+  /**
+   * @param lineBreak @default true
+   */
   static done(lineBreak: boolean = true) {
     Print.ln('✅', lineBreak)
   }
